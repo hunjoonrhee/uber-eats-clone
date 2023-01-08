99c72aaff4629563bd02723e21485dc15dd8c5ec
hunjoonrhee <hunjoonrhee90@gmail.com>
2023-01-08 09:29:05 +0100

    create unit test for jwt, mail service

--- backend/src/jwt/jwt.service.ts
+++ backend/src/jwt/jwt.service.ts
@@ -5,14 +5,13 @@ import {CONFIG_OPTIONS} from '../common/common.constants';
 
 @Injectable()
 export class JwtService {
-  constructor(@Inject(CONFIG_OPTIONS) private readonly options: JwtInterfaces) {
-    console.log(options);
-  }
-  sign(userId: number): string {
-    return jwt.sign({id: userId}, this.options.privateKey);
-  }
+    constructor(@Inject(CONFIG_OPTIONS) private readonly options: JwtInterfaces) {}
+    sign(userId: number): string {
+        return jwt.sign({id: userId}, this.options.privateKey);
+    }
 
-  verify(token: string) {
-    return jwt.verify(token, this.options.privateKey);
-  }
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    verify(token: string) {
+        return jwt.verify(token, this.options.privateKey);
+    }
 }
